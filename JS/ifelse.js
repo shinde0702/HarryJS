3fc9dd9b4a479dab6fcb3f4ecbf70337e278c63c
shinde0702 <abhijeetshinde596@gmail.com>
2022-11-21 13:38:06 +0530

    add file

---
+++ JS/ifelse.js
@@ -0,0 +1,21 @@
+// let age='100'
+
+// if(age==19){
+//     console.log("Age is 19")
+// }
+
+// else if(age==65){
+//     console.log("age is not 19")
+// }
+// else{
+//     console.log("age is not match")
+// }
+let age=18;
+let doesdrive=true;
+if(doesdrive && 18>=age){
+    console.log(" you can drive")
+}
+else{
+   console.log("you can not drive")
+}
+
