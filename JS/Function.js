817364bef8d8726416ad530584177caf2ae850d2
shinde0702 <abhijeetshinde596@gmail.com>
2022-11-23 09:31:41 +0530

    add

--- JS/Function.js
+++ JS/Function.js
@@ -1,1 +1,13 @@
 document.write("Heloo Welcome To Lear Function")
+
+let num1,num2,num3,sum;
+
+num1=10
+num2=30
+sum=num1+num2
+
+document.write("<h1>Addition of two no is : " ,sum ,"</h1>")
+
+sum=num1*num2
+
+document.write("<h1> Multiplication of two no is : ",sum,"</h1>")
