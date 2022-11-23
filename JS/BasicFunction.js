403ba02967761579e3785f152aee6d4e040daaf8
shinde0702 <abhijeetshinde596@gmail.com>
2022-11-23 12:43:08 +0530

    save file

--- JS/BasicFunction.js
+++ JS/BasicFunction.js
@@ -1,13 +1,20 @@
 document.write("Heloo Welcome To Lear Function")
 
 let num1,num2,num3,sum;
+function abhijeet() //i. function Name(abhijeet)
 
-num1=10
-num2=30
+//{ } ii.Function  body defination
+{
+num1= Number( prompt("Enter value for num2"))
+num2= Number(prompt("Enter value for num2")) 
 sum=num1+num2
 
 document.write("<h1>Addition of two no is : " ,sum ,"</h1>")
-
+num1=2
+num2=3
 sum=num1*num2
 
 document.write("<h1> Multiplication of two no is : ",sum,"</h1>")
+}
+abhijeet(); //--Function Calling//
+
---
+++ JS/Simple_Parametered_ReturnFunction.js
@@ -0,0 +1,12 @@
+let num1,num2,num3,sum;
+
+function parameterized(n1,n2)
+
+{
+
+return(n1+n2)
+
+
+}
+
+document.write(parameterized(10,20))
