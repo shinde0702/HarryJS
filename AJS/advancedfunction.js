ae2feb4836373c65b43d869841ad6f3b08dd9c24
shinde0702 <abhijeetshinde596@gmail.com>
2022-11-23 13:27:48 +0530

    add file

---
+++ AJS/advancedfunction.js
@@ -0,0 +1,25 @@
+// Session-01 Advanced Function
+// i.By Expression Function--> When we store function in  variable (i.e let expression = function fun(){body} and called by also same variable (we can use any name of variable).
+
+
+/*let expression = function fun(){
+    document.write(" This is By Expression Function")
+}
+expression();      */
+
+
+// ii.Ananoymous Function--> no need to take function name /Without name function.
+
+/*let num = function(){
+
+    document.write("This is Ananoymous Function")
+}
+num(); */
+
+
+
+// iii.Arrow Function--> no need to write function keyword, function name also not required, if single statement then no need to use bracess curely bracket,  need to use Arrow like this =>  .
+
+/*let arr=()=> document.write("This is Arrow type function")
+
+arr(); //calling */
